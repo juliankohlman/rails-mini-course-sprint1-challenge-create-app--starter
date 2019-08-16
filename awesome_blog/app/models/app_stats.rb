f3f3a080d0eb9ede000de4f8021326f38a4ff4fb
Julian Kohlman <juliankohlman@gmail.com>
2019-08-16 11:56:01 -0400

    Tests routes and AppStats class with rails console

--- awesome_blog/app/models/app_stats.rb
+++ awesome_blog/app/models/app_stats.rb
@@ -14,15 +14,20 @@ class AppStats
 	def quotes_count
 		@quotes.count
 	end
+
 	def posts_word_count
 		@posts.map { |p| p.body.split.length }.reduce(:+)
 	end
+
 	def quotes_word_count
 		@quotes.map { |q| q.body.split.length }.reduce(:+)
 	end
+
 	def total_count
 		self.posts_count + self.quotes_count
 	end
+
+	# nils can't be coerced so if either of the values don't have any data yet a TypeError will get thrown
 	def total_word_count
 		self.posts_word_count + self.quotes_word_count
 	end
