e1efb252d020c8008870c89a5cacb70798d52b13
Julian Kohlman <juliankohlman@gmail.com>
2019-08-16 12:31:21 -0400

    Adds seed.rb file with bob ross lipsum

--- awesome_blog/db/seeds.rb
+++ awesome_blog/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(
+	title: 'Ruby',
+	body:
+		"You don't have to spend all your time thinking about what you're doing, you just let it happen. Let all these things just sort of happen.
+"
+)
+Post.create(
+	title: 'Ruby on Rails',
+	body: "Isn't it great to do something you can't fail at? It is a lot of fun.
+
+"
+)
+Post.create(
+	title: 'Gems',
+	body:
+		"We'll put all the little clouds in and let them dance around and have fun. Those great big fluffy clouds.
+
+"
+)
+Post.create(
+	title: 'Bob Ross would have liked RoR',
+	body: 'Nothing wrong with washing your brush. Let the paint work.
+'
+)
+Quote.create(body: 'Take your time. Speed will come later.')
+Quote.create(body: 'Just think about these things in your mind - then bring them into your world.
+')
+Quote.create(body: 'Let\'s do that again.')
+Quote.create(body: 'Maybe there\'s a little something happening right here.')
